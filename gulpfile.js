e6a75d37ca5c5106aad18027cf3bde9e896fddab
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-01-03 14:56:08 -0700

    tests

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var through = require('through2');
+
+function unique() {
+  return gulp.src(['./src/file.js', './src/**/*.js'])
+    .pipe(through.obj(function(file, _, cb) {
+      console.log(file);
+      cb(null, file);
+    }))
+    .pipe(gulp.dest('./dist'))
+}
+
+exports.default = unique;
