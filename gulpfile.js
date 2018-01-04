39d7a6841746ad1c0413fc5aebc334838a0156e2
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-01-04 12:04:34 -0700

    uniqueby to debug

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,12 @@ var gulp = require('gulp');
 var through = require('through2');
 
 function unique() {
-  return gulp.src(['./src/./file.js', './src/./**/*.js'])
+  return gulp.src(['./src/./file.js', './src/./**/*.js'], {
+    uniqueBy: function(globj) {
+      console.log(arguments);
+      return globj.path;
+    }
+  })
     .pipe(through.obj(function(file, _, cb) {
       console.log(file.path);
       cb(null, file);
